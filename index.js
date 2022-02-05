a16f5d716ff9393fd2146031eb0f41706ba50bf3
MD ADIL ALAM <16951253+adilalam@users.noreply.github.com>
2022-02-05 17:18:46 +0530

    add isNullOrEmpty function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hi there');
+function isNullOrEmpty(input) {
+  // Returns true if the input is either undefined, null, or empty, false otherwise
+  return input === undefined || input === null || input === '';
+}
+// Export to make the function available to other packages
+module.exports = isNullOrEmpty;
